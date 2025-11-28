ca5d2b89aaefad1f0cb35622b6ddc67974d04898
dihubcn <dihubcn@gmail.com>
2025-11-28 21:56:43 +0800

    getStoredMessages limit

--- lib/src/stores/message-store.dart
+++ lib/src/stores/message-store.dart
@@ -49,9 +49,9 @@ class MessageStore with StoreMixin {
     await db.delete(kTableName);
   }
 
-  Future<List<DataMessage>> query(String topic) async {
+  Future<List<DataMessage>> query(String topic, {int limit = 20}) async {
     final maps = await db.query(kTableName,
-        where: '$kColumnTopic = ?', whereArgs: [topic], orderBy: kColumnSeq);
+        where: '$kColumnTopic = ?', whereArgs: [topic], orderBy: '$kColumnSeq desc', limit: limit);
     return maps.map((row) => _convert(row)).toList();
   }
 
--- lib/src/topic.dart
+++ lib/src/topic.dart
@@ -652,9 +652,9 @@ class Topic {
     return _messages.getLast();
   }
 
-  Future<List<DataMessage>?> getStoredMessages() async {
+  Future<List<DataMessage>?> getStoredMessages({int limit = 20}) async {
     if (name == null) return null;
-    return _databaseManager.message.query(name!);
+    return _databaseManager.message.query(name!, limit:limit);
   }
 
   Future<DataMessage?> getLastStoredMessage() async {
